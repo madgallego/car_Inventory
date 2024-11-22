c1df4f2af4fa19a155566584b66e2d012207ec2a
Kaizu-r <jljoloc@gmail.com>
2024-11-22 17:58:39 +0800

    Added: Truck.java

---
+++ Truck.java
@@ -0,0 +1,41 @@
+package oop.car;
+
+import oop.car.Car;
+
+public class Truck extends Car{
+    private boolean style;  //bed style. true for stepside, false for fleetside
+    private int size;   //1 for kei, 2 for compact, 3 for mid-size, 4 for full-size
+    public static int count = 0;
+
+    public Truck(String ID){
+        super(ID);
+        count++;
+    }   
+
+    public boolean getStyle(){
+        return style;
+    }
+    public String getStyleString(){ //returns style as a string
+        if(style)
+            return "Stepside";
+        return "Fleetside";
+    }
+    public int getSize(){
+        return size;
+    }
+    public String getSizeString(){  //returns size as a string
+        switch(size){
+            case 1:
+                return "Kei";
+            case 2:
+                return "Compact";
+            case 3:
+                return "Mid-size";
+            case 4:
+                return "Full-size";
+            default:
+                return "Null";
+        }
+    }
+
+}
