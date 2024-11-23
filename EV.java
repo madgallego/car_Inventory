8b46e04966a3f31e6f8f6224a4219872a0b097ea
Kaizu-r <jljoloc@gmail.com>
2024-11-23 19:14:23 +0800

    UPD: added Electric cars class

---
+++ EV.java
@@ -0,0 +1,43 @@
+package oop.car;
+
+import oop.car.Car;
+
+public class EV extends Car{
+    private int range;  //mileage before out of battery
+    private char connector; //a for type 1, b for type 2, c for wireless
+    private int batteryLife;    //battery life before it needs to be swapped/replaced
+    public static int count = 0;
+
+    public EV(String ID){
+        super(ID);
+        count++;
+    }
+    public int getRange(){
+        return range;
+    }
+    public int getRangeKMS(){
+        return (int) ((float) range * 1.6);
+    }
+    public char getConnector(){
+        return connector;
+    }
+    public String getConnectorStr(){
+        switch(connector){
+            case 'a':
+                return "Type 1";
+            case 'b':
+                return "Type 2";
+            case 'c':
+                return "Wireless/Electric Road";
+            default:
+                return "null";
+
+        }
+    }
+    public int getBatteryLife(){
+        return batteryLife;
+    }
+    public int getBatteryLifeKMS(){
+        return (int) (((float) batteryLife )* 1.6);
+    }
+}
