7b1ad1d429a31543cbdc988b3097361ff0cd9080
Kaizu-r <jljoloc@gmail.com>
2024-12-08 13:51:18 +0800

    RMV: Removed offroad attribute since all suvs are offroad (duh)

--- oop/car/SUV.java
+++ oop/car/SUV.java
@@ -1,18 +1,11 @@
 package oop.car;
 
 public class SUV extends Car{
-    private boolean offroad;
     private char style; //a crossover/ b mini/ c compact/ d mid/ e full/ f extended/ g coupe
     public static int count = 0;
     public SUV(String ID){
         super(ID);
         count++;
-    }
-    public void setOffRoad(boolean offroad){
-        this.offroad = offroad;
-    }
-    public boolean getOffRoad(){
-        return this.offroad;
     }
     public char getStyle(){
         return this.style;
