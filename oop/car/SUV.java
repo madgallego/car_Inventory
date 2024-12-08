8834bf9ac5e6ac1c40973f9c80ac226288d63a0d
Kaizu-r <jljoloc@gmail.com>
2024-12-08 13:49:05 +0800

    UPD: Changed style from string to char. Made relevant methods for it too

--- oop/car/SUV.java
+++ oop/car/SUV.java
@@ -2,7 +2,7 @@ package oop.car;
 
 public class SUV extends Car{
     private boolean offroad;
-    private String style;
+    private char style; //a crossover/ b mini/ c compact/ d mid/ e full/ f extended/ g coupe
     public static int count = 0;
     public SUV(String ID){
         super(ID);
@@ -14,8 +14,31 @@ public class SUV extends Car{
     public boolean getOffRoad(){
         return this.offroad;
     }
-    public String getStyle(){
+    public char getStyle(){
         return this.style;
+    }
+    public void setStyle(char style){
+        this.style = style;
+    }
+    public String getStyleStr(){
+        switch(style){
+            case 'a':
+                return "Crossover";
+            case 'b':
+                return "Mini";
+            case 'c':
+                return "Compact";
+            case 'd':
+                return "Mid";
+            case 'e':
+                return "Full";
+            case 'f':
+                return "Extended-lenght";
+            case 'g':
+                return "Coupe";
+            default:
+                return "null";
+        }
     }
 
 }
