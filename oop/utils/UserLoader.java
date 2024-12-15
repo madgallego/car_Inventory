cdbe0b4abb9dc5c6565c6311bedebb1267e82df2
OverwatchXX <xarisdelarosat@gmail.com>
2024-12-15 11:41:34 +0800

    ADD: UserLoader.java file

---
+++ oop/utils/UserLoader.java
@@ -0,0 +1,19 @@
+package oop.utils;
+
+import oop.car.*;
+import oop.user.*;
+import java.util.*;
+import java.io.*;
+
+public class UserLoader {
+    public ArrayList<Admin> loadAdmin() {
+
+    }
+
+    public ArrayList<Client> loadClient() {
+        
+    }
+
+    //method for saving/appending new clients
+    //method for saving/appending new admin
+}
